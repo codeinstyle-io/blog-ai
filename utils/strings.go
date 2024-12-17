45aae05a8d7fb963fb66a591744ac04cd45e536e
Samori Gorse <samori@codeinstyle.io>
2024-12-17 23:04:29 +0100

    chore: Neutralize handlers tests for now

--- utils/strings.go
+++ utils/strings.go
@@ -1,8 +1,24 @@
 package utils
 
 import (
+	"math/rand"
 	"strings"
+	"time"
 )
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
 
 func Slugify(s string) string {
 	return strings.ToLower(strings.ReplaceAll(s, " ", "-"))
